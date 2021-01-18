02fdb7e965346baba37da9426e27a41e98e49f01
ayato-ininin <sarai.a.1203408@gmail.com>
2021-01-18 10:22:50 +0900

    ad

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
+use Intervention\Image\Facades\Image;
+use App\Services\CheckExtensionServices;
+use App\Services\FileUploadServices; 
+
+
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -24,16 +29,36 @@ class CreateNewUser implements CreatesNewUsers
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
+             
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
-            'self_introduction' => $data['self_introduction'],
-            'sex' => $data['sex'],
-            'img_name' => $data['img_name'],
+       'self_introduction' => ['string', 'max:255'],
+            'sex' => $input['sex'],
+           'img_name' => ['file', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2000'],
+
+
         ])->validate();
+
+        //引数 $data から name='img_name'を取得(アップロードするファイル情報)
+        $imageFile = $input['img_name'];
+
+        $list = FileUploadServices::fileUpload($imageFile);
+         list($extension, $fileNameToStore, $fileData) = $list;
+        $data_url = CheckExtensionServices::checkExtension($fileData, $extension); 
+
+        //画像アップロード(Imageクラス makeメソッドを使用)
+        $image = Image::make($data_url);
+
+        //画像を横400px, 縦400pxにリサイズし保存
+        $image->resize(400,400)->save(storage_path() . '/app/public/images/' . $fileNameToStore );
+        // ---ここまで追加---
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'self_introduction' => $input['self_introduction'],
+            'sex' => $input['sex'],
+            'img_name' => $fileNameToStore,
         ]);
     }
 }
